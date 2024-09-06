5efc0dc02c2b3da98b0b3f809c15c61f862e7caf
whei2419 <joelnacionales24@gmail.com>
2024-09-06 23:12:57 +0800

    add congrats bubble

--- resources/views/congrats.blade.php
+++ resources/views/congrats.blade.php
@@ -1,10 +1,12 @@
 <x-app-layout>
-    <div class="container-fluid home start completed-screen pt-4">
-
-        </div>
+    <div class="pt-4 container-fluid home start completed-screen congrats-page">
         <div class="col-12 d-flex justify-content-center">
             @include('components.branding')
         </div>
-
+        <div class="mt-3 image-container">
+            @for ($i = 1; $i <= 3; $i++)
+                <img class="logo image image-{{ $i }}" src="{{ asset('images/result' . $i . '.png') }}" alt="">
+            @endfor
+        </div>
     </div>
 </x-app-layout>
