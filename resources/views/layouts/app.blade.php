e8a40c2acb0f8830002998c5795e3717151c10d1
johncarlo2020 <johncarlocasipit@gmail.com>
2024-09-06 15:44:08 +0000

    kose

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-        <title>{{ config("app.name", "Rohto") }}</title>
+        <title>{{ config("app.name", "Kose") }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net" />
