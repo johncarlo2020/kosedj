6d9f1eea2ca541a63b7d274a896ab1ec82ca9b1a
whei2419 <87625823+whei2419@users.noreply.github.com>
2024-09-08 15:00:38 +0800

    re style

--- resources/views/congrats.blade.php
+++ resources/views/congrats.blade.php
@@ -43,6 +43,7 @@
             text-decoration: none;
             color: #fff;
         }
+
     </style>
     <div class="pt-4 container-fluid home start completed-screen congrats-page">
         <div class="col-12 d-flex justify-content-center">
@@ -52,18 +53,43 @@
             Well done {{ auth()->user()->fname }} ! <br />
             Head to the ticket counter for your skin consultation
         </h1>
-        <div class="mt-3 image-container">
+        <div class="mt-3 ranking-container">
             @foreach ($top as $index => $item)
-                <img class="logo image image-{{ $index + 1 }}"
-                    src="{{ asset('images/result' . $item['survey'] . '.png') }}" alt="" />
+                <div class="ranking ranking__{{$item['survey']}} mb-2 {{ $index >= 3 ? 'd-none' : '' }}">
+                    <img src="{{ asset('images/result' . $item['survey'] . '.png') }}" alt="">
+                    <div class="barholder">
+                        <p class="rank-name">Blood Deficient</p>
+                        <div class="progress">
+                            <div class="progress-bar" roley="progressbar" style="width: {{ $item['percentage_answered'] }}%;" aria-valuenow="{{$item['percentage_answered']}}" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <p class="rank-points">{{ $item['count'] }}/{{ $item['total'] }}</p>
+                    </div>
+                </div>
             @endforeach
+        </div>
+        <div class="btn-container">
+            <a href="#" class="show-more">Show more</a>
         </div>
     </div>
 
-    <footer>
+    <footer class="p-2">
         <p>Visit our official website</p>
         <img class="footer-logo" src="{{ asset('images/logo-large.png') }}" alt="" />
         <br>
         <a href="https://www.kose.com.my">Click Here for more Information</a>
     </footer>
+
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const showMoreBtn = document.querySelector('.show-more');
+            const hiddenItems = document.querySelectorAll('.ranking.d-none');
+
+            showMoreBtn.addEventListener('click', function (e) {
+                e.preventDefault();
+                hiddenItems.forEach(item => item.classList.remove('d-none'));
+                showMoreBtn.style.display = 'none';
+            });
+        });
+    </script>
 </x-app-layout>
