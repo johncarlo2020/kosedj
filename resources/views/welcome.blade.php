93bf788fa741c84fa15337adb190659ff8edbf3b
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2025-04-08 13:55:28 +0800

    change 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,9 +55,9 @@
                     @include('components.branding')
                 </div>
                 <div class="mt-3 text-center col-12 text-content welcome">
-                    <h1 class="mt-5 heading">Where Wellness Embraces Beauty</h1>
+                    <h1 class="mt-5 heading">A new Form of</h1>
                     <h1 class="mt-2 heading">
-                        Japanese Holistic Kampology Skincare
+                        Brightening Begins
                     </h1>
                     <div class="mt-3 branding">
                         <img
